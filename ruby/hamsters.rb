21ebddfb6d8ec00447f463b56a0244b9adc6bb6d
Rose Kelly <lrkelly09@gmail.com>
2016-11-15 20:41:52 -0600

    Change strings to integers and print answers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,7 +2,7 @@ puts "What's the name of this hamster?"
 name = gets.chomp
 
 puts "On a scale from 1-10, how noisy is this hamster?"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 
 puts "What is this hamster's fur color?"
 fur_color = gets.chomp
@@ -11,14 +11,14 @@ puts "Is this hamster a good candidate for adoption?"
 adoption_candidate = gets.chomp
 
 puts "How old is this hamster?"
-age = gets.chomp
+age = gets.chomp.to_i
   if age == ""
     age = nil
   end
 
 
-p name
-p volume_level
-p fur_color
-p adoption_candidate
-p age
+p "This hamster's name is #{name}."
+p "On a volume scale of 1-10, this hamster is a #{volume_level}."
+p "This hamster has #{fur_color} fur."
+p "Is this hamster a good candidate for adoption? Answer: #{adoption_candidate}."
+p "Estimated age of this hamster: #{age}."
