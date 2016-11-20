951a58e4ea57f2fbbc3c7402c17b3066a0f637a8
Rose Kelly <lrkelly09@gmail.com>
2016-11-19 19:23:21 -0600

    Finsihing vampires and commiting vampires for now

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,10 +1,11 @@
-# define the encrypt method
-# begin the letter_count at 0 (first letter of the input string)
-# start the loop: while letter_counter is less than the string length,
-# if it's "a" change it to "b" and so on <-- this did not work but
-# "z" to "a" did.
-# if it's a space, change nothing
-# add one letter each time so you're always looking at the next one
+# define encrypt method and pass a string into it
+# set letter_counter to 0 (first letter of the input string)
+# Use a loop? While letter_counter is less than the string.length,
+# if the letter_counter is equal to "a" set it to "b"
+# Spaces can remain spaces
+# Then go to the next letter letter
+# Add 1 to the letter_counter each time
+# print the new encrypted string
 
 
 def encrypt(string)
@@ -30,7 +31,55 @@ encrypt("zed")
 # start the letter_counter at 0 again? Or -1?
 # while the letter_counter is less than the string length,
 # if the string's letter_counter is at "z" change it to "y"
-#
+
+# if you have a letter, and want to get the previous letter 
+# find the index of the current letter, -= 1 each time for the
+# index of the previous letter
+# then search thru the alphabet for the specific index
+
+
+def decrypt(string)
+  letter_counter = 0
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  while letter_counter < string.length
+    if string[letter_counter] == "a"
+      string[letter_counter] = "z"
+    elsif string[letter_counter] == " "
+      string[letter_counter]
+    else 
+
+      new_index = alphabet.index(string[letter_counter])
+      string[letter_counter] = alphabet[new_index -1]
+    end
+    letter_counter += 1
+  end
+  p string
+end
+
+    
+    # other attempts... ignore
+    # current_letter = string[letter_counter]
+    #  current_index = alphabet.index(current_letter)
+    #  previous_index = current_index - 1
+    #  alphabet = previous_index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def decrypt(string)
--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,42 +1,87 @@
-puts "What is your name?"
+puts "How many new employees will you be entering?"
+new_employees = gets.chomp.to_i
+
+until new_employees == 0
+
+puts "what's your name?"
 name = gets.chomp
 if name == "Drake Cula"
   name = false
+elsif name == "Tu Fang"
+  name = false
+else name == true
 end
 
 puts "How old are you?"
 age = gets.chomp.to_i
-if age < 100 
+if age < 100
   age = true
-else age = false
+else
+  age = false
 end
 
 puts "What year were you born?"
-year = gets.chomp.to_i
+birth_year = gets.chomp.to_i
 
-puts "Would you like some garlic bread? (yes/no)"
+puts "Would you like garlic bread? (yes/no)"
 wants_bread = gets.chomp
 if wants_bread == "yes"
   wants_bread = true
-else wants_bread = false
+else
+  wants_bread = false
 end
 
-puts "Would you like to enroll in health insurance? (yes/no)"
-insurance = gets.chomp
-if insurance == "yes"
+puts "Would you like health insurance? (yes/no)"
+insuarnce = gets.chomp
+if insuarnce == "yes"
   insurance = true
-else insurance = false
+else
+  insurance = false
 end
+
+
+
+
+
+puts "Do you have any allergies? Please list one at a time and type 'done' when finished."
+done = false
+  until done == true
+    allergies = gets.chomp
+    if allergies == "done"
+      done = true
+    elsif allergies == "sunshine"
+      sunshine_allergy = true
+      done = true
+    else done = false
+    end
+  end
 
 
 case 
-when age == true && (wants_bread == true || insurance == true)
-  puts "Probably not a vampire."
-when age == false && (wants_bread == false && insurance == false)
-  puts "Almost certainly a vampire."
-when age == false && (wants_bread == false || insurance == false)
+when sunshine_allergy == true
   puts "Probably a vampire."
 when name == false
   puts "Definitely a vampire."
+when age == false && wants_bread == false && insurance == false
+  puts "Almost certainly a vampire."
+when age == true && (wants_bread == true || insurance == true)
+  puts "Probably not a vampire."
+when age == false && (wants_bread == false || insurance == false)
+  puts "Probably a vampire."
+else puts "Results inconclusive." 
+end
+
+puts "Name: #{name}"
+puts "Age is acceptable: #{age}"
+puts "Birth year: #{birth_year}"
+puts "Would like garlic bread: #{wants_bread}"
+puts "Would like insurance: #{insurance}"
+
+new_employees -= 1
 
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+# Ask user how many employees will be processed
+# After first is done, start survey and repeat until done
